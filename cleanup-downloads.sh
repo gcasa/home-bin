70513b9005f907b5ab206d7537836dfbc170595b
Gregory John Casamento <greg.casamento@gmail.com>
2022-04-17 03:23:19 -0400

    Update cleanup script to clean the desktop as well

--- cleanup-downloads.sh
+++ cleanup-downloads.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo "Cleaning up..."
+echo "Cleaning up... Downloads"
+echo " "
 
 pushd ~/Downloads
 echo " "
@@ -17,17 +18,54 @@ mv 2> /dev/null *.zip* ~/Archives
 mv 2> /dev/null *.tar* ~/Archives
 mv 2> /dev/null *.dmg* ~/Archives
 mv 2> /dev/null *.iso* ~/Archives
+mv 2> /dev/null *.pkg* ~/Archives
 
-# documents
+# Documents
 echo "Documents..."
 mv 2> /dev/null *.pdf* ~/Archives
 mv 2> /dev/null *.doc* ~/Archives
 mv 2> /dev/null *.txt* ~/Archives
 
-# dev...
-mv 2> /dev/null * ~/Development
+# Apps
+echo "Applications..."
+mv 2> /dev/null *.app ~/Applications
+
+# All else in trash...
+echo "Trash..."
+mv 2> /dev/null * ~/.Trash
 
 echo " "
+popd
+
+echo "Cleaning up... Desktop"
+echo " "
+
+pushd ~/Desktop
+
+# pictures...
+echo "Images/Pictures..."
+mv 2> /dev/null *.jpg* ~/Pictures
+mv 2> /dev/null *.png* ~/Pictures
+mv 2> /dev/null *.tiff* ~/Pictures
+
+# archives
+echo "Archives..."
+mv 2> /dev/null *.zip* ~/Archives
+mv 2> /dev/null *.tar* ~/Archives
+mv 2> /dev/null *.dmg* ~/Archives
+mv 2> /dev/null *.iso* ~/Archives
+mv 2> /dev/null *.pkg* ~/Archives
+
+# Documents
+echo "Documents..."
+mv 2> /dev/null *.pdf* ~/Archives
+mv 2> /dev/null *.doc* ~/Archives
+mv 2> /dev/null *.txt* ~/Archives
+
+# Apps
+echo "Applications..."
+mv 2> /dev/null *.app ~/Applications
+
 popd
 
 echo "Done"
